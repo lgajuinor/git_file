8227611d9733497237b5b9aba3316e63673c8a54
deluo <915716526@qq.com>
2023-03-30 09:30:18 +0800

    新增了裸机开发工程

---
+++ robot_slave/bare_development/SOFTWARE/voltage_get.c
@@ -0,0 +1,7 @@
+#include "voltage_get.h"
+
+
+uint16_t Get_adc(uint32_t ch)
+{
+    
+}
---
+++ robot_slave/bare_development/SOFTWARE/voltage_get.h
@@ -0,0 +1,9 @@
+#ifndef __VOLTAGE_GET_H
+#define __VOLTAGE_GET_H
+
+#include "adc.h"
+uint16_t Get_adc(uint32_t ch);
+
+
+
+#endif
